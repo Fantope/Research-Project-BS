d275eafc6b5080ffb4b85dfad8e5305174deb40e
otcova <otgercomas@gmail.com>
2022-01-13 20:42:55 +0100

    refactoring

--- main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.nav-menu-right').addEventListener('click',() => {
-    document.querySelector('.nav-menu').classList.toggle('show');
-});
-ScrollReveal().reveal('.Principal', {delay:100});
-ScrollReveal().reveal('.Extra', {delay: 250});
