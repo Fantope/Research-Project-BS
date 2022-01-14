3e12b9fe121aec1a1e397db65ecfecde3521bd3f
otcova <otgercomas@gmail.com>
2022-01-14 19:47:15 +0100

    diapo core

---
+++ diapo.js
@@ -0,0 +1,31 @@
+
+const container = document.getElementById("diapo-container")
+
+function getPanels() {
+	return Array.from(container.childNodes).filter(e => e.nodeName == "DIV")
+}
+
+function getCurrentPanelIndex() {
+	const panels = getPanels()
+	let currentPanelIndex = 0
+	let minPanelOffset = Infinity
+	for (let i = 0; i < panels.length; ++i) {
+		const thisPanelOffset = Math.abs(container.scrollLeft + container.offsetLeft - panels[i].offsetLeft)
+		if (thisPanelOffset < minPanelOffset) {
+			minPanelOffset = thisPanelOffset
+			currentPanelIndex = i
+		}
+	}
+	return currentPanelIndex
+}
+
+container.onclick = e => {
+	const panels = getPanels()
+	const currentPanelIndex = getCurrentPanelIndex()
+	if (panels.length == currentPanelIndex) {
+		// Last panel
+	} else {
+		container.scrollLeft = panels[currentPanelIndex + 1].offsetLeft - container.offsetLeft
+	}
+}
+
