acad370faaed3da5f533583f9e2b95a9bcdb19ad
Fantope <a.nicora.sh@gmail.com>
2022-01-14 01:03:24 +0100

    Actualización de tamaño mediano. Acabado de añadir el "nav", se han hecho pruebas de diseño en el archivo "Nerfea" y "Avanzar", primer diseño esstablecido + mejoras de calidad general.

---
+++ Avanzar.js
@@ -0,0 +1,10 @@
+var max="4";
+var actual="0";
+if(document.addEventListener ('mousedown', function(e){})== 1){
+    actual++;
+    console.log("si");
+    if (actual<= max){
+        let container = document.getElementById("container")
+        //XD, ahora a esguir probando
+    }
+}
